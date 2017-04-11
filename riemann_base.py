67b34b0fae76973fe9c95d56296c5e6f2ea77226
Paulo Toro <pauloat@gmail.com>
2017-04-11 11:20:32 -0300

    Update riemann_base.py

--- riemann_base.py
+++ riemann_base.py
@@ -3,11 +3,11 @@ import math
 def fnf(i):  
   return #here's go the function  
  
-a = 1
-b = 3
-numberofsteps = 4
+a = #start
+b = #end
+numberofsteps = #number of steps
 deltax = (b - a) / numberofsteps
-x = a
+x = a # <- for the left, for the middle: + deltax / 2, for the right: + deltax
 accumulation = 0
 
 for x in range(0, numberofsteps):
